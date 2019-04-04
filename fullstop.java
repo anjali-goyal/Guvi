c6285520fd60d92e554c3d82adbc13acbe168ecb
Anjali Goyal <43495770+anjali-goyal@users.noreply.github.com>
2019-04-04 07:41:52 +0530

    Add files via upload

---
+++ fullstop.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+public static void main(String args[]){
+Scanner obj=new Scanner(System.in);
+String st=obj.nextLine();
+st=st+'.';
+System.out.println(st);
+}
+}
